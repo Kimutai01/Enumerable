5d67e9d721b996193b90b3c032f9ab84a84d9e9c
RitobrotoMukherjee <ritobrotomukherjee1991@gmail.com>
2022-08-19 14:09:16 +0530

    made all? enumerable to block from multiline

--- enumerable.rb
+++ enumerable.rb
@@ -2,9 +2,7 @@ module MyEnumerable
   def all?
     return 'No blocks given' unless block_given?
 
-    each do |item|
-      return false unless yield item
-    end
+    each { |item| return false unless yield item }
 
     true
   end
